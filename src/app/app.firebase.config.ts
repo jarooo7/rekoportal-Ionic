522b0de985b89f8a3add068bef6f2521cdf4c7f3
@jarek564as <jarek564as@gmail.com>
2018-12-18 11:46:59 +0100

    start

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebase_config = {
+    apiKey: 'AIzaSyDlHL20-th_hPjOs4ZdpUeer7FnDxbtx4E',
+    authDomain: 'rekoportal.firebaseapp.com',
+    databaseURL: 'https://rekoportal.firebaseio.com',
+    projectId: 'rekoportal',
+    storageBucket: 'rekoportal.appspot.com',
+    messagingSenderId: '1018805222981'
+}
